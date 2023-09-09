f457ca2d3bdceed0c05d3f993a0e9b71255fcfbe
c-ehrlich <ehrlich.christopher@gmail.com>
2023-09-09 23:41:04 +0200

    move regional edge to east2

--- src/app/api/edge/hello-regional-cle1/route.ts
+++ src/app/api/edge/hello-regional-cle1/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export const runtime = 'edge';
-export const preferredRegion = 'iad1'; // us-east-1
+export const preferredRegion = 'cle1'; // us-east-2
 export const dynamic = 'force-dynamic'
 
 export async function GET() {
